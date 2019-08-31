79ab8154177af5788bd997868a8040cf078e072a
Hailey <haileyhe01@gmail.com>
2019-08-31 19:35:06 -0400

    no message

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
 // 이미지 alt 속 내용을 캡션으로 만들어줌
 $('.post > p > img[alt]').replaceWith(function () {
     return '<figure>'
-        + '<a href="' + $(this).attr('http://hailey99.github.io/assets/images/can0.png') + '" class="mg-link">'
-        + '<img src="' + $(this).attr('http://hailey99.github.io/assets/images/can0.png') + '"/></a>'
+        + '<a href="' + $(this).attr('http://hailey99.github.io/assets/images') + '" class="mg-link">'
+        + '<img src="' + $(this).attr('http://hailey99.github.io/assets/images') + '"/></a>'
         + '<figcaption class="caption">' + $(this).attr('alt') + '</figcaption>'
         + '</figure>';
 });
